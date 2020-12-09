e9df0c4f99e155931535d65194a7befd7c4a4b04
seaside2mm <seasidezhang@gmail.com>
2020-12-09 21:35:41 +0900

    add

---
+++ course/02.IMU传感器/第2节代码/vio_data_simulation-master/main/seaside.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+
+#include "../src/imu.h"
+#include "../src/utilities.h"
+
+using namespace std;
+
+std::vector < std::pair < Eigen::Vector4d, Eigen::Vector4d>>
+createPointsLines(vector<Eigen::Vector4d, Eigen::aligned_allocator<Eigen::Vector4d>>& points)
+{
+    std::vector<std::pair<Eigen::Vector4d, Eigen::Vector4d>> lines;
+    std::ve
+}
